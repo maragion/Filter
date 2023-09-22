4eba1fe7ab03cc40c138cb868ef8ed040aec6dcc
Aleksei Taraskin <bakamoena@gmail.com>
2023-09-22 11:56:06 +0700

    Main Components was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterComponent } from './components/filter/filter.component';
+import { TableComponent } from './components/table/table.component';
+import { AccountsComponent } from './pages/accounts/accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    TableComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
