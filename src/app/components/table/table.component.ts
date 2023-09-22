2813abbcccb4ca6cf7e6defc726e53fc4ee292aa
Aleksei Taraskin <bakamoena@gmail.com>
2023-09-22 20:46:49 +0700

    Now data from users array and data array merged in one array. field was addet to table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -3,28 +3,48 @@ import {HttpService} from "../../services/http.service";
 import {User} from "../../interfaces/user";
 import {MatTableModule} from "@angular/material/table";
 import {MatIconModule} from "@angular/material/icon";
-import {NgClass} from "@angular/common";
+import {DatePipe, NgClass, NgIf} from "@angular/common";
 import {MatButtonModule} from "@angular/material/button";
+import {Data} from "../../interfaces/data";
+import {UserData} from "../../interfaces/user-data";
+import {FinalUser} from "../../interfaces/final-user";
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
   standalone: true,
-  imports: [MatTableModule, MatIconModule, NgClass, MatButtonModule],
+  imports: [MatTableModule, MatIconModule, NgClass, MatButtonModule, NgIf, DatePipe],
 })
-export class TableComponent implements OnInit{
+export class TableComponent implements OnInit {
 
   constructor(private http: HttpService) {
   }
 
   users: User[] = [];
+  usersData: UserData[] | null = null;
+
+  usersFinal: FinalUser[]  = []
 
   ngOnInit() {
-    this.http.getData().subscribe((data: any) => this.users = data.users);
+    this.http.getData().subscribe((data: Data) => {
+      this.users = data.users;
+      this.usersData = data.data;
+
+      if (this.usersData) {
+        this.usersData.forEach((userData) => {
+          this.users.forEach((user) => {
+            if (userData.user_id === user.id) {
+              this.usersFinal.push({...user, ...userData})
+            }
+          })
+        })
+      }
+    });
   }
 
-  displayedColumns: string[] = ['actions', 'login', 'email', 'phone'];
+  dataSource = this.usersFinal;
+  displayedColumns: string[] = ['actions', 'login', 'email', 'phone', "is_admin", "update_at", "create_at", "status", "is_ecp"];
 
 }
 
---
+++ src/app/interfaces/final-user.ts
@@ -0,0 +1,12 @@
+export interface FinalUser {
+  "id": number,
+  "name": string,
+  "email": string,
+  "phone": number,
+  "create_at": number,
+  "update_at": number,
+  "user_id": number,
+  "is_admin": boolean,
+  "is_ecp": boolean,
+  "status": string
+}
