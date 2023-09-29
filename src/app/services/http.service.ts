66a82b108518840d4d0d5273dc2233c1a256c62c
Aleksei Taraskin <bakamoena@gmail.com>
2023-09-29 18:19:58 +0700

    create environment.ts change some styles

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Data} from "../interfaces/data";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  url: string = "https://cars.cprogroup.ru/api/rubetek/angular-testcase-list/"
+  private readonly url: string = `${environment.apiUrl}/rubetek/angular-testcase-list/`
   getData() {
     return this.http.get<Data>(this.url);
   }
