36df25cb44bc229587cf78bd1068b147485053bf
Aleksei Taraskin <bakamoena@gmail.com>
2023-09-29 13:26:04 +0700

    navigation component was made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,17 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {DatePipe} from "@angular/common";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {NgxMaskDirective, provideEnvironmentNgxMask} from "ngx-mask";
+import { NavigationComponent } from './components/navigation/navigation.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
     AppComponent,
     FilterComponent,
     AccountsComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,7 +38,10 @@ import {NgxMaskDirective, provideEnvironmentNgxMask} from "ngx-mask";
     MatTableModule,
     ReactiveFormsModule,
     FormsModule,
-    NgxMaskDirective
+    NgxMaskDirective,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatSelectModule
   ],
   providers: [DatePipe, provideEnvironmentNgxMask()],
   exports: [
